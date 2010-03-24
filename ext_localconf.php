3b09d13fd00f49eff267eea00a90cb5524738287
stoefln <stoefln@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-03-24 14:41:57 +0000

    code cleanup fixed a bug where the port was not read by the extension, therefore other ports than 25 were not working
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/stoefln_pear_mail/trunk@31449 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,6 +6,7 @@ $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]['smtpServer'] = $_EXTCONF['smtp
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]['smtpUser'] = $_EXTCONF['smtpUser'] ? $_EXTCONF['smtpUser'] : '';
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]['smtpPassword'] = $_EXTCONF['smtpPassword'] ? $_EXTCONF['smtpPassword'] : '';
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]['smtpDebug'] = $_EXTCONF['smtpDebug'] ? $_EXTCONF['smtpDebug'] : '';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]['smtpPort'] = $_EXTCONF['smtpPort'] ? $_EXTCONF['smtpPort'] : '';
 
 $TYPO3_CONF_VARS['FE']['XCLASS']['t3lib/class.t3lib_htmlmail.php'] = t3lib_extMgm::extPath($_EXTKEY)."class.ux_t3lib_htmlmail.php";
 $TYPO3_CONF_VARS['BE']['XCLASS']['t3lib/class.t3lib_htmlmail.php'] = t3lib_extMgm::extPath($_EXTKEY)."class.ux_t3lib_htmlmail.php";
